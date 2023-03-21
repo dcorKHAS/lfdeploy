c10e044aece6f6907174b0cfbadbbe6dcf82485e
dcorus <dogan.corus@gmail.com>
2023-03-21 15:50:05 +0300

    Update mailConf.sh

--- mailConf.sh
+++ mailConf.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Install Exim
-sudo apt-get update
-sudo apt-get install exim4 -y
+# Install Exim4
+apt update
+apt install exim4 -y
 
-# Configure Exim
-sudo sed -i 's/dc_eximconfig_configtype=.*/dc_eximconfig_configtype=\'internet\'/g' /etc/exim4/update-exim4.conf.conf
-sudo sed -i 's/dc_local_interfaces=.*/dc_local_interfaces=\'\'/g' /etc/exim4/update-exim4.conf.conf
-sudo sed -i 's/dc_readhost=.*/dc_readhost=\'$HOSTNAME\'/g' /etc/exim4/update-exim4.conf.conf
-sudo sed -i 's/dc_relay_domains=.*/dc_relay_domains=\'*\'/g' /etc/exim4/update-exim4.conf.conf
-sudo sed -i 's/dc_minimaldns=.*/dc_minimaldns=\'false\'/g' /etc/exim4/update-exim4.conf.conf
-sudo sed -i 's/dc_relay_nets=.*/dc_relay_nets=\'\'/g' /etc/exim4/update-exim4.conf.conf
-sudo sed -i 's/dc_smarthost=.*/dc_smarthost=\'\'/g' /etc/exim4/update-exim4.conf.conf
+# Generate Exim4 configuration
+dpkg-reconfigure exim4-config
 
-# Restart Exim
-sudo service exim4 restart
+# Backup original Exim4 configuration
+cp /etc/exim4/update-exim4.conf.conf /etc/exim4/update-exim4.conf.conf.orig
+
+# Configure Exim4 to listen on all interfaces and send mail directly
+sed -i 's/dc_eximconfig_configtype=.*/dc_eximconfig_configtype="internet"/' /etc/exim4/update-exim4.conf.conf
+sed -i 's/dc_other_hostnames=.*/dc_other_hostnames="localhost"/' /etc/exim4/update-exim4.conf.conf
+sed -i 's/dc_local_interfaces=.*/dc_local_interfaces='\\''0.0.0.0'\\''/' /etc/exim4/update-exim4.conf.conf
+sed -i 's/dc_readhost=.*/dc_readhost='\\'''/ /etc/exim4/update-exim4.conf.conf
+sed -i 's/dc_smarthost=.*/dc_smarthost='\\'''/ /etc/exim4/update-exim4.conf.conf
+sed -i 's/dc_use_split_config=.*/dc_use_split_config='\\''false'\\''/' /etc/exim4/update-exim4.conf.conf
+
+# Apply Exim4 configuration
+update-exim4.conf
+
+# Restart Exim4
+systemctl restart exim4.service
