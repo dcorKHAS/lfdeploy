447940e363fde608e64b0876cf81c4f90ca8dec2
dcorus <dogan.corus@gmail.com>
2023-03-21 15:23:49 +0300

    Update mailConf.sh

--- mailConf.sh
+++ mailConf.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y exim4
+# Install Exim
+sudo apt-get update
+sudo apt-get install exim4 -y
 
-sed -i 's/dc_eximconfig_configtype=.*/dc_eximconfig_configtype=\'internet\'/' /etc/exim4/update-exim4.conf.conf
-sed -i 's/dc_local_interfaces=.*/dc_local_interfaces=\'0.0.0.0\'/' /etc/exim4/update-exim4.conf.conf
+# Configure Exim
+sudo sed -i 's/dc_eximconfig_configtype=.*/dc_eximconfig_configtype=\'internet\'/g' /etc/exim4/update-exim4.conf.conf
+sudo sed -i 's/dc_local_interfaces=.*/dc_local_interfaces=\'\'/g' /etc/exim4/update-exim4.conf.conf
+sudo sed -i 's/dc_readhost=.*/dc_readhost=\'$HOSTNAME\'/g' /etc/exim4/update-exim4.conf.conf
+sudo sed -i 's/dc_relay_domains=.*/dc_relay_domains=\'*\'/g' /etc/exim4/update-exim4.conf.conf
+sudo sed -i 's/dc_minimaldns=.*/dc_minimaldns=\'false\'/g' /etc/exim4/update-exim4.conf.conf
+sudo sed -i 's/dc_relay_nets=.*/dc_relay_nets=\'\'/g' /etc/exim4/update-exim4.conf.conf
+sudo sed -i 's/dc_smarthost=.*/dc_smarthost=\'\'/g' /etc/exim4/update-exim4.conf.conf
 
-hostname=$(hostname)
-
-sed -i "s/dc_relay_domains=.*/dc_relay_domains=\'$hostname:localhost\'/" /etc/exim4/update-exim4.conf.conf
-sed -i "s/dc_other_hostnames=.*/dc_other_hostnames=\'$hostname\'/" /etc/exim4/update-exim4.conf.conf
-
-update-exim4.conf
-service exim4 restart
+# Restart Exim
+sudo service exim4 restart
